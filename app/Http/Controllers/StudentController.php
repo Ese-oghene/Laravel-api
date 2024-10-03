baade5ebe529e12f3859f0f2df594fa21c02903c
Ese-oghene <oghenee08@gmail.com>
2024-10-03 12:48:00 +0100

    second push

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -51,4 +51,50 @@ class StudentController extends Controller
    
         }
     }
+
+    public function edit(Request $request, $id){
+        $validator = Validator::make($request->all(), [
+            'name' => 'required',
+            'email' => 'required|email'
+       ]);
+       
+       if ($validator->fails()) {
+       $data = [
+           "status"=>422,
+           "message"=>$validator->messages() 
+       ];
+
+         return response()->json($data, 422);
+       }else {
+           $student = Student::find($id);
+
+           $student->name=$request->name;
+           $student->email=$request->email;
+           $student->phone=$request->phone;
+
+           $student->save();
+
+           $data=[
+               'status'=>200,
+               'message'=>'Data updated successfully'
+           ];
+
+           return response()->json($data, 200);
+  
+       }
+
+    }
+
+    public function delete(Request $request, $id){
+        $student= Student::find($id);
+        $student->delete();
+        
+        $data=[
+            'status'=>200,
+            'message'=>'Data updated successfully'
+        ];
+
+
+        return response()->json($data, 200);
+    }
 }
